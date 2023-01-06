8371d66fd1e3370432b75fc9106422a23ffe1aa9
komar1811 <ostap.komar7@gmail.com>
2023-01-06 03:32:59 +0100

    first commit

--- helpers.py
+++
@@ -1,9 +0,0 @@
-from constants import sh_kom
-
-
-def get_kicked():
-    kick_sheet = sh_kom.get_worksheet(1).get("A1:A50")
-    kick_players = []
-    for player in kick_sheet:
-        kick_players.append(player[0])
-    return kick_players
